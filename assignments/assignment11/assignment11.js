c31d91ef32f6998131866e71918f1f2a488bbc97
Lily Battin <89280985+pollbite@users.noreply.github.com>
2022-11-07 20:04:41 -0700

    finished assignment
    did some basic css, fixed issue with button

--- assignments/assignment11/assignment11.js
+++ assignments/assignment11/assignment11.js
@@ -1,13 +1,11 @@
 
-const pokemonButton = document.querySelector('new-pokemon');
+const pokemonButton = document.querySelector('.new-pokemon');
 
 const callPokemon = getRandomPokemon();
 
-pokemonButton.addEventListener('click', getRandomPokemon());
+pokemonButton.addEventListener('click', getRandomPokemon);
 
 Window.onload = getRandomPokemon();
-
-//console.log(getRandomPokemonNumber());
 
 // function gets a random pokemon and calls the renderPokemon function to render it
 function getRandomPokemon() {
@@ -15,11 +13,9 @@ function getRandomPokemon() {
     fetch(`https://pokeapi.co/api/v2/pokemon/${number}`)
     .then(response => response.json())
     .then(function(data){
-        var name = data.species.name; 
-
-        // this is the old call for black and white sprites
-        //var sprite = data.versions.generation_v.black_white.animated.front_default;
-
+        var pokemonName = data.species.name; 
+        // this changes the first letter to upper case 
+        var name = pokemonName[0].toUpperCase() + pokemonName.substring(1);
         var sprite = data.sprites.front_default;
         renderPokemon(name, sprite);
     });
@@ -31,95 +27,10 @@ function getRandomPokemonNumber() {
     return Math.floor(Math.random() * (905 - 1) + 1);
 }
 
+// function replaces pokemon name and image elements with new random pokemon
 function renderPokemon(name, sprite){
     document.getElementById('js-pokemon-name').textContent = name; 
     document.getElementById('js-pokemon-image').src = sprite;
 }
-
-
-//getRandomPokemon(getRandomPokemonNumber());
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// old pokemon code 
-/*
- const pokemonArray = [];
-
-// credit to https://medium.com/@sergio13prez/fetching-them-all-poke-api-62ca580981a2 for information on how to use pokemon api
-
-function fetchPokemon() {
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=649')
-    .then(response => response.json())
-    .then(function(allpokemon) {
-        allpokemon.results.forEach(function(pokemon){
-            fetchPokemonData(pokemon);
-        })
-    })
-}
-
-function fetchPokemonData(pokemon){
-    let url = pokemon.url
-    fetch (url)
-    .then (response => response.json())
-    .then(function(pokeData) {
-        pushPokemon(pokeData)
-    })
-}
-
-function pushPokemon(pokeData) {
-    pokemonArray.push(pokeData);
-}
-
-
-// credit to https://www.programiz.com/javascript/examples/get-random-item for base of this function
-function randomPokemonSelector(arr) {
-    
-    const randomIndex = Math.floor(Math.random() * arr.length);
-
-    const pokemon = arr[randomIndex];
-
-    return pokemon;
-
-    
-}
-
-fetchPokemon();
-console.log(pokemonArray);
-
-const randomPokemon = randomPokemonSelector(pokemonArray);
-console.log(randomPokemon); 
-*/
-
 
 
