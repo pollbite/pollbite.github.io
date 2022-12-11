a7b3b7b0a2994aec896c35a5522dfb6e7dfd1125
Lily Battin <89280985+pollbite@users.noreply.github.com>
2022-12-10 17:39:28 -0700

    having issues with class names, im gonna go back and give everything ids

--- js/script.js
+++ js/script.js
@@ -1,101 +1,98 @@
-// color button
-document
-  .querySelector(".no-button")
-  .addEventListener("click", console.log(tester));
 
-  function tester() {
-    return console.log("I'm listening");
-  }
+document.addEventListener("DOMContentLoaded", function () {
 
-// these are the elements with the color white
-let webFile = document.getElementsByClassName('file');
-let highlightAboutBox = document.getElementsByClassName('highlight-about-box');
-let highlightBox = document.getElementsByClassName('highlight-box');
+    // color button
+    var colorButton = document.getElementById("btnColor");
+    colorButton.addEventListener("click", colorChanger());
 
-// these are the elements with the darkest color, #23006e
-let webBody = document.querySelector('body');
-let webH2 = document.querySelector('h2');
-let webH3 = document.querySelector('h3');
-let webH4 = document.querySelector('h4');
-let webH5 = document.querySelector('h5');
-let webH6 = document.querySelector('h6');
-let webHeaderLink = document.getElementsByClassName('header-link');
+    // these are the elements with the color white
+    var webFile = document.getElementsByClassName('file');
+    var highlightAboutBox = document.getElementsByClassName('highlight-about-box');
+    var highlightBox = document.getElementsByClassName('highlight-box');
+
+    // these are the elements with the darkest color, #23006e
+    var webBody = document.querySelector('body');
+    var webH2 = document.querySelector('h2');
+    var webH3 = document.querySelector('h3');
+    var webH4 = document.querySelector('h4');
+    var webH5 = document.querySelector('h5');
+    var webH6 = document.querySelector('h6');
+    var webHeaderLink = document.getElementsByClassName('header-link');
 
 
-// these are the elements with the midtone color, #1f68b8;
-let webIcon = document.querySelector('i');
-let webLink = document.querySelector('a');
-let unhighlightAboutBox = document.getElementsByClassName('unhighlight-about-box');
-let unhighlightBox = document.getElementsByClassName('unhighlight-box');
+    // these are the elements with the midtone color, #1f68b8;
+    var webIcon = document.querySelector('i');
+    var webLink = document.querySelector('a');
+    var unhighlightAboutBox = document.getElementsByClassName('unhighlight-about-box');
+    var unhighlightBox = document.getElementsByClassName('unhighlight-box');
 
-// these are the elements with the purple color, #b515c2
-let webBorder = document.querySelector('hr');
-let webDottedBorder = document.getElementsByClassName('line');
+    // these are the elements with the purple color, #b515c2
+    var webBorder = document.querySelector('hr');
+    var webDottedBorder = document.getElementsByClassName('line');
 
-// these are the elements with the lightest color, #fac632
-let webH1 = document.querySelector('h1');
+    // these are the elements with the lightest color, #fac632
+    var webH1 = document.querySelector('h1');
 
 
 
-function getRandomColor() {
-    let characters = "0123456789ABCDEF";
-    let color = "#";
 
-    for (let i=0; i<6; i++) {
-        color += characters[getRandomNumber(0,15)];
+    function getRandomColor() {
+        let characters = "0123456789ABCDEF";
+        let color = "#";
+
+        for (let i=0; i<6; i++) {
+            color += characters[getRandomNumber(0,15)];
+        }
+
+        return color;
+
     }
 
-    return color;
+    function getRandomNumber(low, high) {
+        let r = Math.floor(Math.random() * (high - low + 1)) + low; 
+        return r;
+    }
 
-}
+    /*function colorChangeWhite(color){
+        webFile[0].style.color = color;
+        webFile[0].style.border = color;
 
-function getRandomNumber(low, high) {
-    let r = Math.floor(Math.random() * (high - low + 1)) + low; 
-    return r;
-}
+        highlightAboutBox[0].style.backgroundColor = color;
+        highlightBox[0].style.backgroundColor = color; 
+    }*/
 
-function colorChangeWhite(color){
-    webFile.style.color = color;
-    webFile.style.border = color;
+    function colorChangeDarkest(color){
+        /*webBody[0].style.backgroundColor = color; */
+        webH2[0].style.color = color; 
+        webH3[0].style.color = color; 
+        webH4[0].style.color = color; 
+        webH5[0].style.color = color; 
+        webH6[0].style.color = color; 
+        webHeaderLink[0].style.color = color;
+    }
 
-    highlightAboutBox.style.backgroundColor = color;
-    highlightBox.style.backgroundColor = color; 
-}
+    function colorChangeMidtone(color){
+        webIcon[0].style.color = color; 
+        webLink[0].style.color = color; 
+        unhighlightAboutBox[0].style.backgroundColor = color; 
+        unhighlightBox[0].style.backgroundColor = color;
+    }
 
-function colorChangeDarkest(color){
-    webBody.style.backgroundColor = color;
-    webH1.style.color = color; 
-    webH2.style.color = color; 
-    webH3.style.color = color; 
-    webH4.style.color = color; 
-    webH5.style.color = color; 
-    webH6.style.color = color; 
-    webHeaderLink.style.color = color;
-}
+    function colorChangePurple(color){
+        webBorder[0].style.color = color; 
+        webDottedBorder[0].style.color = color;
+    }
 
-function colorChangeMidtone(color){
-    webIcon.style.color = color; 
-    webLink.style.color = color; 
-    unhighlightAboutBox.style.backgroundColor = color; 
-    unhighlightBox.style.backgroundColor = color;
-}
+    function colorChangeLightest(color){
+        webH1[0].style.color = color; 
+    }
 
-function colorChangePurple(color){
-    webBorder.style.color = color; 
-    webDottedBorder.style.color = color;
-}
-
-function colorChangeLightest(color){
-    webH1.style.color = color; 
-}
-
-function colorChanger(){
-    colorChangeWhite(getRandomColor());
-    colorChangeDarkest(getRandomColor());
-    colorChangeMidtone(getRandomColor());
-    colorChangePurple(getRandomColor());
-    colorChangeLightest(getRandomColor());
-}
+    function colorChanger(){
+        colorChangeDarkest(getRandomColor());
+        colorChangeMidtone(getRandomColor());
+        colorChangePurple(getRandomColor());
+        colorChangeLightest(getRandomColor());
+    }
 
 
-
+});
