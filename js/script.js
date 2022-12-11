a38081874c15d07f6dd437dca17b06225d748c90
Lily Battin <89280985+pollbite@users.noreply.github.com>
2022-12-10 22:25:16 -0700

    i tried a bunch of different games but none of them quiet worked. gonna come back tommorow

--- js/script.js
+++ js/script.js
@@ -1,3 +1,40 @@
 $(document).mousemove(function(e){
     $('.cursor-image').stop().animate({left:e.pageX, top:e.pageY});
 });
+
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+
+function jump() {
+  if (dino.classList != "jump") {
+    dino.classList.add("jump");
+
+    setTimeout(function () {
+      dino.classList.remove("jump");
+    }, 300);
+  }
+}
+
+let isAlive = setInterval(function () {
+  // get current dino Y position
+  let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+
+  // get current cactus X position
+  let cactusLeft = parseInt(
+    window.getComputedStyle(cactus).getPropertyValue("left")
+  );
+
+  // detect collision
+  if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
+    // collision
+    alert("Game Over!");
+  }
+}, 10);
+
+document.addEventListener("keydown", function (event) {
+  jump();
+});
+
+
+
+
