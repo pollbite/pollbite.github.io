bb839cb3ab15a59bb7bab05082f2189586f724ba
Lily Battin <89280985+pollbite@users.noreply.github.com>
2022-12-10 23:30:49 -0700

    final edits to document - formatting, comments on js

--- js/cursor.js
+++ js/cursor.js
@@ -1,4 +1,4 @@
-$(document).mousemove(function(e){
-    $('.cursor-image').stop().animate({left:e.pageX, top:e.pageY});
+// cursor is no a seaparate js file because for some reason the code interferes with the javascript game
+$(document).mousemove(function (e) {
+  $(".cursor-image").stop().animate({ left: e.pageX, top: e.pageY });
 });
-  
--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
+const butterfree = document.getElementById("butterfree");
+const tree = document.getElementById("tree");
 
-const dino = document.getElementById("dino");
-const cactus = document.getElementById("cactus");
-
+// jump function, clears the added jump animation class after 300 sec
 function jump() {
   if (butterfree.classList != "jump") {
     butterfree.classList.add("jump");
@@ -12,38 +12,36 @@ function jump() {
   }
 }
 
+// adds animation for the moving trees to the tree classlist so that it won't animate on launch
 function treeJump() {
   if (tree.classList != "tree-jump") {
-    tree.classList.add("tree-jump")
+    tree.classList.add("tree-jump");
   }
 }
 
+// game rules, collision detection
+let isAlive = setInterval(function () {
+  // get current butterfree Y position
+  let butterfreeTop = parseInt(
+    window.getComputedStyle(butterfree).getPropertyValue("top")
+  );
 
-  let isAlive = setInterval(function () {
-    // get current dino Y position
-    let butterfreeTop = parseInt(window.getComputedStyle(butterfree).getPropertyValue("top"));
-  
-    // get current cactus X position
-    let treeLeft = parseInt(
-      window.getComputedStyle(tree).getPropertyValue("left")
-    );
-  
-    // detect collision
-    if (treeLeft < 50 && treeLeft > 0 && butterfreeTop >= 140) {
-      // collision
-      alert("Game Over!");
-      tree.classList.remove("tree-jump");
-    }
-  }, 10);
+  // get current tree X position
+  let treeLeft = parseInt(
+    window.getComputedStyle(tree).getPropertyValue("left")
+  );
 
+  // detect collision
+  if (treeLeft < 50 && treeLeft > 0 && butterfreeTop >= 140) {
+    // collision
+    alert("Game Over!");
+    tree.classList.remove("tree-jump");
+  }
+}, 10);
 
-
-
+// event listeneres, click for start game, keydown for jumps
 document.addEventListener("keydown", function (event) {
   jump();
 });
 
 document.addEventListener("click", treeJump);
-
-
-
